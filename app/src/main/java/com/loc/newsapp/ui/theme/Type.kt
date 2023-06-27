53869e4acbb9d33b881a4460a8522f0e2208fd08
Mohammad nawas <mohammednawas8@gmail.com>
2023-06-27 15:42:48 +0300

    poppins_semibold

--- app/src/main/java/com/loc/newsapp/ui/theme/Type.kt
+++ app/src/main/java/com/loc/newsapp/ui/theme/Type.kt
@@ -13,6 +13,7 @@ val Poppins = FontFamily(
     fonts = listOf(
         Font(R.font.poppins_regular, FontWeight.Normal),
         Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold),
     )
 )
 
@@ -49,5 +50,5 @@ val Typography = Typography(
         fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         lineHeight = 19.sp,
-    )
+    ),
 )
