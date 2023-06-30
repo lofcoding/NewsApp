c38f202f9ac77ba79d023b860f5fb01f676b2a47
mac <samerammar@alMacBook-Air-alkhas-bsamer.local>
2023-06-30 06:49:21 -0700

    AppModule

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.loc.newsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -17,15 +18,23 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var useCases: AppEntryUseCases
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         installSplashScreen()
         setContent {
             NewsAppTheme(
---
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.loc.newsapp.di
+
+import android.app.Application
+import com.loc.newsapp.data.manger.LocalUserMangerImpl
+import com.loc.newsapp.domain.manger.LocalUserManger
+import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
+import com.loc.newsapp.domain.usecases.app_entry.ReadAppEntry
+import com.loc.newsapp.domain.usecases.app_entry.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManger(
+        application: Application
+    ): LocalUserManger = LocalUserMangerImpl(context = application)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(
+        localUserManger: LocalUserManger
+    ): AppEntryUseCases = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+
+}
