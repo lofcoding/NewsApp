ec0153cf71da3578f04ff96758820d5f42cd7c0a
Mohammad Nawas <mohammed8.nawas@gmail.com>
2023-07-07 17:16:56 +0300

    Add resources

--- app/src/main/java/com/loc/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/loc/newsapp/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val DarkRed = Color(0xFFC30052) //Dark Error
 val LightRed = Color(0xFFFF84B7)
 
 val LightBlack = Color(0xFF3A3B3C) //Dark Surface
+
+
+val BlueGray = Color(0xFFA0A3BD)
+val WhiteGray = Color(0xFFB0B3B8)
