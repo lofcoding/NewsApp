1c55e8cabcbce75f1ea4655d7153ec3824410afc
MuKomy <mustafaelkomy2000@gmail.com>
2024-02-07 08:37:52 +0200

    ..

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 dynamicColor = false
             ) {
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    //OnBoardingScreen()
+                    OnBoardingScreen()
                 }
             }
         }
